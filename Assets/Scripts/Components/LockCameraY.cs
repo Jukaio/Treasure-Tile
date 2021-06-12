603b63ea574fc3bd9d493a7e390d0ea4ca4ae340
David Naußed <jukaio@live.de>
2021-06-12 17:34:37 +0300

    Added: A convenient camera to explore the whole level instead of just seeing a portion of it!

---
+++ Assets/Scripts/Components/LockCameraY.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockCameraY : MonoBehaviour
+{
+    // Not a pretty camera, but at least it is not jumping with the player
+    // It solves its purpose for now!
+    private float y = 0.0f;
+    void Start()
+    {
+        y = Camera.main.transform.position.y;
+    }
+
+    void LateUpdate()
+    {
+        var position = Camera.main.transform.position;
+        position.y = y;
+        Camera.main.transform.position = position;
+    }
+}
